9f264f341cd635779758c7b5fedf7bb66e738de8
Ray Imber <ray.imber@galavantier.com>
2014-03-30 18:46:33 -0700

    Created a custom multiplex server with socket.io because the built in multiplex server didn't work for me.

---
+++ multiplex-server/multiplexer-server.js
@@ -0,0 +1,31 @@
+var port = 1948;
+var io = require('socket.io').listen(port);
+console.log('listening on Port: ' + port);
+
+var slideIndeces = { h: 0, v: 0, f:-1 };
+var codeIsHighlighted = false;
+io.sockets.on('connection', function (socket) {
+    // sync up any new clients
+    socket.emit('sync', slideIndeces);
+    if(codeIsHighlighted) {
+        socket.emit('toggleHighlightCode');
+    }
+
+    // if the master has connected, use her slide Indeces and re-sync everybody up.
+    socket.on('masterSync', function(data){
+        slideIndeces = data;
+        io.sockets.emit('sync', slideIndeces);
+    });
+    socket.on('masterToggleHighlightCode', function(){
+        codeIsHighlighted = !codeIsHighlighted;
+        io.sockets.emit('toggleHighlightCode');
+    });
+
+    // login the Master
+    socket.on('masterLogin', function(data){
+        // not very secure lol.
+        if(data == 'cs780') {
+            socket.emit('masterLoginSuccess');
+        }
+    });
+});
